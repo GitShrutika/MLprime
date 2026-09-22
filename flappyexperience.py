2c9f1a114204291d2346c01bf5e4ae9455159b61
Shrutika <shrutika.gitdesk@gmail.com>
2026-09-22 18:03:48 +0530

    Implement ReplayMemory class for experience replay

---
+++ flappyexperience.py
@@ -0,0 +1,16 @@
+from collection import deque
+import random
+
+class ReplayMemory():
+    def __init__(self,maxlen,seed=None):
+        self.memory=deque([],maxlen=maxlen)
+
+    def append(self ,new_exp):
+        self.memory.append(new_exp)
+
+    def sample(self,sample_size):  
+        return random.sample(self.memory,random_size)
+
+    def __len__(self):
+        return len(self.memory)  
+
